efefeef2a960212cd1001391e79e9a83e4896ed3
jhon <jhon861213@gmil.com>
2023-05-24 22:15:49 -0400

    segundo parcial

---
+++ script_comentarios.js
@@ -0,0 +1,17 @@
+ 
+
+/* No te preocupes si no funciona bien.
+Si todo estuviera correcto, serías despedido de tu trabajo.
+Y no habria mucho que aprender.*/
+
+
+// Primero resuelve el problema. Luego, escribe el código.
+
+
+/* Cualquier código tuyo que no hayas mirado en los últimos seis meses o más,
+es como si lo hubiese escrito otra persona
+
+-- Eagleson's Law */
+
+
+
